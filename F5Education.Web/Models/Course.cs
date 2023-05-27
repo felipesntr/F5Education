9a0a3a07f8b695ca8ddcb815722ddaa14e35ecb1
Felipe Santos <rfelipe273@gmail.com>
2023-05-27 16:22:21 -0300

    first commit

---
+++ F5Education.Web/Models/Course.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace F5Education.Web.Models
+{
+    public class Course
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        [JsonPropertyName("school_name")]
+        public string SchoolName { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<Course>(this);
+    }
+}
